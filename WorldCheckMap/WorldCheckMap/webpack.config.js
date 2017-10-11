6f5a25d0391e957852fa7f6764de25d2616783a6
Roman Chaley <romanchaley@gmail.com>
2017-10-11 17:58:27 +0300

    Added more webpack stuff

---
+++ WorldCheckMap/WorldCheckMap/webpack.config.js
@@ -0,0 +1,36 @@
+﻿const path = require('path');
+
+
+module.exports = {
+    entry: [
+        path.join(__dirname, 'app/start.jsx')
+    ],
+    output: {
+        path: path.join(__dirname, 'wwwroot'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            babelrc: false,
+                            presets: [
+                                'es2015',
+                                'react'
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(css|less)$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            }
+        ]
+    }
+};
